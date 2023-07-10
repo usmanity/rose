1d0c723d76976e676a7ddf161f6d7ba4c56f98c8
Muhammad Usman <contact@mhmd.us>
2023-07-10 12:13:23 -0700

    adding some routing setup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
   return (
     <div className="mx-auto max-w-3xl py-12 md:flex md:items-center md:justify-between">
       <div className="flex justify-center space-x-6 md:order-2">
-        IG Other stuff
+        {/* Instagram Twitter */}
       </div>
       <div className="mt-8 md:order-1 md:mt-0">
         <p className="text-center text-xs leading-5 text-gray-500">
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 type ProjectDataType = {
   name: string;
   role: string;
@@ -6,12 +8,14 @@ type ProjectDataType = {
 
 export function ProjectCard({ data }: { data: ProjectDataType }) {
   return (
-    <div className="mt-4 dark:bg-gray-900 dark:text-white overflow-hidden rounded-lg bg-white shadow">
-      <div className="px-4 pt-5">
-        <img src={`/public/images/${data.cover}`} />
+    <Link to={`/project/${data.name}`}>
+      <div className="mt-4 dark:bg-gray-900 dark:text-white overflow-hidden rounded-lg bg-white shadow">
+        <div className="px-4 pt-5">
+          <img src={`/public/images/${data.cover}`} />
+        </div>
+        <div className="px-4 py-2">{data.name}</div>
+        <div className="px-4 pt-1 pb-3">{data.role}</div>
       </div>
-      <div className="px-4 py-2">{data.name}</div>
-      <div className="px-4 pt-1 pb-3">{data.role}</div>
-    </div>
+    </Link>
   );
 }
